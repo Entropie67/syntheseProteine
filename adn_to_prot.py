ade8ee8278c7683bd4f0f4177b2025a6e0f066c4
Olivier <olivier.elophe@gmail.com>
2021-05-18 13:31:38 +0200

    arn_m

--- adn_to_prot.py
+++ adn_to_prot.py
@@ -1,6 +1,3 @@
-# A - adénine | C - cytosine | G - guanine | T - thymine | U - uracile
-BASES = {'A', 'T', 'C', 'G', 'U'}
-
 # Alanine
 # Arginine
 # Asparagine
@@ -26,8 +23,18 @@ BASES = {'A', 'T', 'C', 'G', 'U'}
 # Nous allons utiliser le "tableau inverse" dans l'article de Wikipédia sur le code génétique
 ACIDES_AMINES = {}
 
+# A - adénine | C - cytosine | G - guanine | T - thymine | U - uracile
+BASES = ('A', 'T', 'C', 'G', 'U')
+COMPLEMENT = ('T', 'A', 'G', 'C')
+# Un dictionnaire nous donnant la correspondance des bases
+COMPLEMENT_DICO = dict(zip(BASES, COMPLEMENT))
+print(COMPLEMENT_DICO)
 # Nous allons partir sur un brin de 64, attention, pas de U dans l'ADN !!
-adn = []
+#
+from random import choice
+adn = [choice(BASES[:-1]) for _ in range(16)]
+print(f"Notre brin ADN : \n{adn}")
+
 
 def arn_m(adn, initial, final):
     """
@@ -36,4 +43,13 @@ def arn_m(adn, initial, final):
         A  -> U
         C <-> G
     """
+    sequence = adn[initial + 1: final + 2]
+    print(sequence)
+    arn_m = [COMPLEMENT_DICO[b] if b != "A" else "U" for b in sequence]
+    return arn_m
+
+print(f"L'arn messagé est :\n{arn_m(adn, 2, 13)}")
+
+
+def traduction(arn_m):
     pass
